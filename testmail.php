587fd3816d094f9720ee7ad4f8c31ca18ae19ae8
Talha Zia <talhazia00@gmail.com>
2016-02-27 21:28:48 -0500

    domain updated

--- testmail.php
+++ testmail.php
@@ -9,19 +9,20 @@ use Mailgun\Mailgun;
 
 # First, instantiate the SDK with your API credentials and define your domain.
 $mg = new Mailgun("key-3f57a39ea3bef42f294c5e5590e6e5d4");
-$domain = "http://ec2-52-36-33-133.us-west-2.compute.amazonaws.com/testmail.php";
+$domain = "https://api.mailgun.net/v3/sandbox06544ae5e2c74187bd56ca729517bc90.mailgun.org";
 
 # Now, compose and send your message.
-$mg->sendMessage($domain, array('from'    => 'talha.zia@uoit.net',
+$result = $mg->sendMessage($domain, array('from'    => 'talha.zia@uoit.net',
                                 'to'      => 'talhazia00@gmail.com',
                                 'subject' => 'The PHP SDK is awesome!',
                                 'text'    => 'It is so simple to send a message.'));
 
-                                $httpResponseCode = $result->http_response_code;
+              $httpResponseCode = $result->http_response_code;
                                 $httpResponseBody = $result->http_response_body;
 
                                 # Iterate through the results and echo the message IDs.
-                                $logItems = $result->http_response_body->items;
+                                /*$logItems = $result->http_response_body->items;
                                 foreach($logItems as $logItem){
                                     echo $logItem->message_id . "\n";
-                                }
+                                }*/
+                                print_r($httpResponseBody->message);
